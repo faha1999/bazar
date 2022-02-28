97c7ea4165b09288de3ba20c09f29d0ac48b686e
faha1999 <kafahad1999@gmail.com>
2022-03-01 00:53:58 +0600

    review comlete

--- app/js/app.js
+++ app/js/app.js
@@ -70,3 +70,26 @@ var swiper = new Swiper('.product-slider', {
     }
   }
 });
+
+// review slider
+
+var swiper = new Swiper('.review-slider', {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1020: {
+      slidesPerView: 3
+    }
+  }
+});
