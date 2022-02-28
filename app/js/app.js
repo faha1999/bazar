0ccb80033bc8ae444bd4e372b37ce0e5eb874748
faha1999 <kafahad1999@gmail.com>
2022-03-01 00:15:31 +0600

    product added

--- app/js/app.js
+++ app/js/app.js
@@ -48,3 +48,25 @@ window.onscroll = () => {
   loginForm.classList.remove('active');
   navbar.classList.remove('active');
 };
+
+// product slider
+var swiper = new Swiper('.product-slider', {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1020: {
+      slidesPerView: 3
+    }
+  }
+});
